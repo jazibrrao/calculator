3a3e32533345412f4f7c41e564683bcb93f1edcf
JR <jazibrrao@gmail.com>
2023-05-20 02:23:01 -0400

    Add functionality for equals

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let a;
 let b;
 let operator;
 const display = document.getElementById("display");
+const operators = ["+", "-", "/", "*"];
 
 function add(a,b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a,b) {
@@ -20,7 +21,20 @@ function divide(a,b) {
 }
 
 function operate(operator, a, b) {
-    return operator(a,b);
+    switch(operator) {
+        case "+":
+            return add(a,b);
+            break;
+        case "-":
+            return subtract(a,b);
+            break;
+        case "*":
+            return multiply(a,b);
+            break;
+        case "/":
+            return divide(a,b);
+            break;
+    }
 }
 
 function populateDisplay(btn) {
@@ -28,5 +42,17 @@ function populateDisplay(btn) {
 }
 
 function clearDisplay() {
-    display.value = " ";
+    display.value = "";
+}
+
+function equals() {
+    //take the display and split into array with operators
+    let e = display.value.split(/(\+|-|\*|\/)/);
+    //solve for 1st 3 items in array
+    a = operate(e[1], e[0], e[2]);
+    //remove 1st 3 items and insert solved number at start of array
+    numberArray.splice(0,3,a);
+    //join the array into a string and update display
+    let newDisplay = e.join('');
+    display.value = newDisplay;
 }
